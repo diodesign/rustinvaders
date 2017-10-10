e6aa6e7599b25be3b7e1765589adead75822351a
Chris Williams <diodesign@gmail.com>
2017-10-09 20:04:15 -0700

    bring in the Rust code

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+/* Space invaders in Rust
+ *
+ * Game concept by Tomohiro Nishikado / Taito
+ * Rust code By Chris Williams <diodesign@gmail.com>
+ *
+ * Written for fun. See LICENSE.
+ *
+ */
+
+fn main()
+{
+  println!("Hello, world!");
+}
+
